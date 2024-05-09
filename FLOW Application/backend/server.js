8746d5ff31eab5216d4e617aecfff4e5fd43719c
Evgeny Leonov <leonov.e@praguecityuniversity.cz>
2024-05-09 18:17:41 +0200

    refactoring

--- FLOW Application/backend/server.js
+++ FLOW Application/backend/server.js
@@ -1,22 +1,19 @@
-
 const express = require('express');
 const path = require('path');
-const ipValidationMiddleware = require('./middleware'); // Import the middleware
+const {checkIp} = require("./ipcheck");
+//const ipValidationMiddleware = require('./middleware'); // Import the middleware
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 // Middleware to use IP validation
-app.use(ipValidationMiddleware);
+app.use(checkIp);
 
 // Endpoint to handle incoming requests
 app.get('/', (req, res) => {
     res.send('Welcome!');
 });
-
-
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
-
 
 // Start server
 app.listen(PORT, () => {
