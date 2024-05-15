a6754d506aa665413401465377f3a9c969ae47a5
Dea <122489106+drrecaj1@users.noreply.github.com>
2024-05-15 18:21:50 +0200

    Update server.js

--- FLOW Application/backend/server.js
+++ FLOW Application/backend/server.js
@@ -5,7 +5,7 @@ const {checkIp} = require("./ipcheck");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 // Middleware to use IP validation
 app.use(checkIp);
